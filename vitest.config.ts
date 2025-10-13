3370dad2fc276946eb8972d4b48eceb6fac5f4af
Chris Manson <chris@manson.ie>
2025-10-13 11:50:12 +0100

    double timeout for windows tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,10 @@ const ONE_SECOND = 1_000;
 
 export default defineConfig({
   /**
-   * add a 3x factor for windows tests... because windows 😭
+   * add a 6x factor for windows tests... because windows 😭
    */
   test: {
-    testTimeout: 240 * ONE_SECOND * (process.platform === 'win32' ? 3 : 1),
-    hookTimeout: 240 * ONE_SECOND * (process.platform === 'win32' ? 3 : 1),
+    testTimeout: 240 * ONE_SECOND * (process.platform === 'win32' ? 6 : 1),
+    hookTimeout: 240 * ONE_SECOND * (process.platform === 'win32' ? 6 : 1),
   },
 });
