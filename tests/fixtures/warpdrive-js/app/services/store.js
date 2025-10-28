dea9dc972aa1acde8736a16760075e47d2409451
Chris Thoburn <cthoburn@auditboard.com>
2025-10-28 14:43:25 -0700

    add tests and fix lint

---
+++ tests/fixtures/warpdrive-js/app/services/store.js
@@ -0,0 +1,32 @@
+import { useLegacyStore } from '@warp-drive/legacy';
+import { JSONAPICache } from '@warp-drive/json-api';
+import { withDefaults } from '@warp-drive/legacy/model/migration-support';
+
+const Store = useLegacyStore({
+  linksMode: true,
+  cache: JSONAPICache,
+  handlers: [
+    {
+      async request() {
+        return {
+          data: {
+            type: 'person',
+            id: '1',
+            attributes: { name: 'Luke Skywalker' }
+          }
+        }
+      }
+    }
+  ],
+  schemas: [
+    // -- your schemas here
+    withDefaults({
+      type: 'person',
+      fields: [
+        { kind: 'field', name: 'name' }
+      ],
+    })
+  ],
+});
+
+export default Store;
