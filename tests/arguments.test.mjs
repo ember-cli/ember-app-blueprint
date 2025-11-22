2baaa50fc9093bc6412dbddd7f81e9faed7dff2f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-22 16:38:15 -0500

    Add tests

--- tests/arguments.test.mjs
+++ tests/arguments.test.mjs
@@ -147,6 +147,40 @@ describe('Blueprint Arguments', function () {
     });
   });
 
+  describe('--node-version', async function () {
+    let currentMajor = process.version.split('.')[0].replace('v', '');
+
+    it('default node is the current node', async function () {
+      const { files } = await generateApp({
+        name: 'foo',
+      });
+
+      expect(files['babel.config.mjs']).not.includes(`import.meta.dirname`);
+      expect(files['.github/workflow/ci.yml']).includes(
+        `node-version: ${currentMajor}`,
+      );
+    });
+
+    it('--node-version sets engine and CI nodes', async function () {
+      const { files } = await generateApp({
+        name: 'foo',
+        flags: ['--node-version=22'],
+      });
+
+      expect(parse(files['package.json']).engines.node).toBe('>= 22');
+      expect(files['.github/workflow/ci.yml']).includes(`node-version: 22`);
+    });
+
+    it('--node-version >= 22.16 uses import.meta.dirname', async function () {
+      const { files } = await generateApp({
+        name: 'foo',
+        flags: ['--node-version=22.16'],
+      });
+
+      expect(files['babel.config.mjs']).includes(`import.meta.dirname`);
+    });
+  });
+
   describe('--no-welcome', async function () {
     it('generates an app with the welcome page component by default', async function () {
       const { files } = await generateApp();
