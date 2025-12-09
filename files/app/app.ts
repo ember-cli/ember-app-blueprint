e56bceb08acab9c9fc45e88fa86825690c38e5e4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-12-08 23:35:51 -0500

    Update app.ts

--- files/app/app.ts
+++ files/app/app.ts
@@ -1,15 +1,18 @@
 <% if (warpDrive) { %>import '@warp-drive/ember/install';<% } %>
 import Application from '@ember/application';
 import compatModules from '@embroider/virtual/compat-modules';
-import Resolver from 'ember-resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from '<%= modulePrefix %>/config/environment';
-import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import Resolver from 'ember-resolver';<% if (compat) { %>
+import loadInitializers from 'ember-load-initializers';<% } %>
+import config from '<%= modulePrefix %>/config/environment';<% if (notMinimal) { %>
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';< } %>
 import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
 
+<% if (notMinimal) { %>
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
 }
+<% } %>
+
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
@@ -17,5 +20,6 @@ export default class App extends Application {
   Resolver = Resolver.withModules(compatModules);
   inspector = setupInspector(this);
 }
-
+<% if (compat) { %>
 loadInitializers(App, config.modulePrefix, compatModules);
+<% } %>
