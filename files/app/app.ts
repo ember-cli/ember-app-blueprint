957318d922cc440f6f7f975f78611bf566813865
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-12-08 23:44:39 -0500

    Update app -- wow so messy

--- files/app/app.ts
+++ files/app/app.ts
@@ -1,8 +1,9 @@
-<% if (warpDrive) { %>import '@warp-drive/ember/install';<% } %>
+<% if (warpDrive) { %>import '@warp-drive/ember/install';<% } %><% if (compat) { %>
 import Application from '@ember/application';
 import compatModules from '@embroider/virtual/compat-modules';
-import Resolver from 'ember-resolver';<% if (compat) { %>
-import loadInitializers from 'ember-load-initializers';<% } %>
+import Resolver from 'ember-resolver';
+import loadInitializers from 'ember-load-initializers';<% } else { %>
+  import Application from 'ember-strict-application-resolver';<% } %>
 import config from '<%= modulePrefix %>/config/environment';<% if (notMinimal) { %>
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';< } %>
 import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
@@ -15,10 +16,16 @@ if (macroCondition(isDevelopingApp())) {
 
 
 export default class App extends Application {
-  modulePrefix = config.modulePrefix;
+<% if (compat) {%>  modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules(compatModules);
-  inspector = setupInspector(this);
+<% } %>  inspector = setupInspector(this);
+<% if (noCompat) { %>
+  modules = {
+    ...import.meta.glob('./router', { eager: true }),
+    ...import.meta.glob('./templates/**/*', { eager: true }),
+  }
+<% } %>
 }
 <% if (compat) { %>
 loadInitializers(App, config.modulePrefix, compatModules);
