1852a061afe80d1633fba6a18bd11101ac1c86d1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-05-13 15:50:02 -0400

    Revert "Merge pull request #49 from ember-cli/nvp/minimal-flag-non-default"
    This reverts commit 87dd08bccbe07c40871906c3a27dbcfdaffad14b, reversing
    changes made to a4314a33ae0e977780ea7c4086ed0af05b864cff.

--- files/tests/test-helper.ts
+++ files/tests/test-helper.ts
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 <% if (warpDrive) { %>import '@warp-drive/ember/install';
 <% } %>import Application from '<%= modulePrefix %>/app';
 import config from '<%= modulePrefix %>/config/environment';
+||||||| 87dd08b (Merge pull request #49 from ember-cli/nvp/minimal-flag-non-default)
+<% if (warpDrive) { %>import '@warp-drive/ember/install';<% } %>
+import Application from '<%= modulePrefix %>/app';
+import config<% if (noCompat) {%>, { enterTestMode }<% } %> from '<%= modulePrefix %>/config/environment';
+=======
+<% if (warpDrive) { %>import '@warp-drive/ember/install';<% } %>
+import Application from '<%= modulePrefix %>/app';
+import config from '<%= modulePrefix %>/config/environment';
+>>>>>>> parent of 87dd08b (Merge pull request #49 from ember-cli/nvp/minimal-flag-non-default)
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
